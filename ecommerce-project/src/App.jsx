01a3f73561eceddcd28e1c8a8e20965458a86cf4
keerthi vardhan <keerthivardhan2k@gmail.com>
2025-10-11 15:33:32 +0530

    Use React Router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,8 +1,14 @@
 import { HomePage } from "./pages/HomePage";
+import { Route, Routes } from "react-router";
 import "./App.css";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
+  );
 }
 
 export default App;
