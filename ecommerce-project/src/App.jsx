d8d17e89ddf5796a75b79a8a3b92f9bc544ecf13
keerthi vardhan <keerthivardhan2k@gmail.com>
2025-10-11 16:05:07 +0530

    setup orders page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,13 +1,15 @@
 import { HomePage } from "./pages/HomePage";
 import { Route, Routes } from "react-router";
-import "./App.css";
 import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
     </Routes>
   );
 }
