3fabc46f6c9b4cb4a12240c0df29466066b89159
keerthi vardhan <keerthivardhan2k@gmail.com>
2025-10-12 11:14:28 +0530

    fetch data from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,7 +1,14 @@
 import { Header } from "../components/Header";
 import "./HomePage.css";
 import { products } from "../../starting-code/data/products";
+
 export function HomePage() {
+  fetch("http://localhost:3000/api/products").then((response) => {
+    return response.json().then((data) => {
+      console.log(data);
+    });
+  });
+
   return (
     <>
       <title>Ecommerce Project</title>
